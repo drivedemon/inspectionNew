11c890e17cdc61b6a742e8279b6998b69cc7bb12
drivedemon <superdriveza@hotmail.com>
2019-05-13 13:47:33 +0700

    + Initial

---
+++ ajax_get_trzone.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'dbconnect.php';
+
+// $dpid = $_REQUEST["dpid"];
+
+$sql2= "SELECT * FROM tr_area ORDER BY id ASC";
+
+$result2 = mysqli_query($conn, $sql2);
+
+  while($row2 = mysqli_fetch_array($result2)) {
+    echo"<option value='".$row2["id"]."'>" .$row2["name"]." </option>";
+  }
+
+?>
